fdf8a2bc1b51e89011a6252b849c509d3dcea267
Alex Chernov <alexusblack@gmail.com>
2018-10-25 16:12:54 +1100

    cleaning repository from 0.7.0-0.9.* items

--- scripts/includes/getdirectories.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = function (srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
-}
--- scripts/makeTilesList.js
+++
@@ -1,9 +0,0 @@
-const makeRepositoryItemsList = require('./includes/make-repository-items-list');
-
-const exclusionList = [
-  'icons'
-];
-const itemsPath = '../tile';
-const itemsListPath = itemsPath + '/tiles.json';
-
-makeRepositoryItemsList(itemsPath, exclusionList, itemsListPath);
